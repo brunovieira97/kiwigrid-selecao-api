0ffdb54b5fe2e44be45938ffe95bc7879599b4d7
Bruno Vieira <bruno.vieira97@outlook.com>
2021-03-27 03:46:26 -0300

    resource de vendas

---
+++ src/main/java/com/kiwigrid/resource/VendaResource.java
@@ -0,0 +1,10 @@
+package com.kiwigrid.resource;
+
+import javax.ws.rs.Path;
+
+import com.kiwigrid.model.Venda;
+
+@Path("/vendas")
+public class VendaResource extends RestCrudResource<Venda, Long> {
+	
+}
