3f353da9906aaf45e575e9339244868fb22b7294
Bruno Vieira <bruno.vieira97@outlook.com>
2021-03-27 03:45:40 -0300

    model de venda

---
+++ src/main/java/com/kiwigrid/model/Venda.java
@@ -0,0 +1,44 @@
+package com.kiwigrid.model;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+
+@Entity
+public class Venda extends PanacheEntity {
+	
+	@ManyToOne
+	private Vendedor vendedor;
+	
+	@OneToMany(cascade = CascadeType.ALL)
+	private List<ProdutoVenda> produtos;
+
+	public Venda() {}
+
+	public Venda(Vendedor vendedor, List<ProdutoVenda> produtos) {
+		this.vendedor = vendedor;
+		this.produtos = produtos;
+	}
+
+	public Vendedor getVendedor() {
+		return vendedor;
+	}
+
+	public void setVendedor(Vendedor vendedor) {
+		this.vendedor = vendedor;
+	}
+
+	public List<ProdutoVenda> getProdutos() {
+		return produtos;
+	}
+
+	public void setProdutos(List<ProdutoVenda> produtos) {
+		this.produtos = produtos;
+	}
+
+}
